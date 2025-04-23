f9b5197f71b304825b66c1fb690ead767ec837d1
Aitor Alien <111972684+Alien69Flow@users.noreply.github.com>
2025-04-23 18:53:42 +0200

    Update Logo.tsx

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,20 +1,25 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
     <div className="flex items-center gap-4">
+      {/* Logo principal */}
       <Link to="/" className="flex items-center gap-2">
         <img 
           src="/lovable-uploads/a0f89be6-744a-4898-8955-b41b14629755.png" 
           alt="AlienFlowSpace Logo" 
           className="h-9" 
         />
-        <span className="font-bold text-xl text-alien-gold font-[Atomic Age]">
-          Alien<span className="text-alien-green">Flow</span>
+        <span className="font-bold text-xl">
+          <span className="text-alien-green">Alien</span>
+          <span className="text-alien-gold">Flow</span>
+          <span className="text-alien-green">Space</span>
+          <span className="text-alien-gold">DAO</span>
         </span>
       </Link>
+
+      {/* Icono del planeta */}
       <Link to="/">
         <img 
           src="/lovable-uploads/79900e3c-f502-40b8-a628-e021acfcdb40.png" 
