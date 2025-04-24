fb3ef6cb010c2f02f9b80e0e182c75dd8470ca70
Aitor Alien <111972684+Alien69Flow@users.noreply.github.com>
2025-04-24 21:23:57 +0200

    Update ConnectButton.tsx

--- src/components/Header/ConnectButton.tsx
+++ src/components/Header/ConnectButton.tsx
@@ -6,7 +6,7 @@ const ConnectButton = () => {
   return (
     <Button className="bg-alien-green hover:bg-alien-green-light text-alien-gold font-[Atomic Age] rounded-full flex items-center gap-2">
       <img 
-        src="/lovable-uploads/e2d45b36-c4c1-4b45-953d-5376a5f5b6cc.png" 
+        src="/lovable-uploads/AW.png" 
         alt="Wallet" 
         className="h-6 w-6 rounded-full" 
       />
